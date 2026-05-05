625b61e1d32553a043a02f11496fa5a56cd143c9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:23:36 +0400

    Added constructor  for  fields in  Class Adrress

--- src/Entites/Adrress.java
+++ src/Entites/Adrress.java
@@ -5,7 +5,9 @@ public class Adrress {
         private String city;
         private String postalCode;
 
-
-
-    
+        public Adrress(String street, String city, String postalCode) {
+                this.street = street;
+                this.city = city;
+                this.postalCode = postalCode;
         }
+}
