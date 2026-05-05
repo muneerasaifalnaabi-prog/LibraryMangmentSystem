d86549936b6d857bf7894d2927c1c6cb6209420f
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 13:36:34 +0400

    added constructor

--- src/Service/BorrowingService.java
+++ src/Service/BorrowingService.java
@@ -4,13 +4,28 @@ import Entites.LibraryItem;
 import Entites.Members;
 
 public class BorrowingService {
-    private LibraryItem libraryItem;
-    private Members members;
+    private LibraryItemService libraryItem;
+    private MemberService members;
 
     public static int MAX_ITEMS=3;
 
-    public BorrowingService(LibraryItem libraryItem, Members members) {
+    public BorrowingService(LibraryItemService libraryItem, MemberService members) {
         this.libraryItem = libraryItem;
         this.members = members;
     }
-}
+
+    public Boolean borrowItem(String memberId, String itemId) {
+        Members m= members.findMemberById(memberId);
+        if (m==null){
+            return false;
+        }
+        LibraryItem item = libraryItem.findBookById(itemId);
+        if (item == null) {
+            return false;
+        }
+        return true;
+
+    }
+
+
+    }
