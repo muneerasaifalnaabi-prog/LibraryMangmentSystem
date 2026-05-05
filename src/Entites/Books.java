7ab1fb33775205ecb5f2d234b80e9954971a2e5e
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:07:23 +0400

    Added getter and setter for  fields in  Class Book

--- src/Entites/Books.java
+++ src/Entites/Books.java
@@ -4,5 +4,19 @@ public class Books {
     private  String author;
     private String isbn;
 
+    public String getAuthor() {
+        return author;
+    }
 
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
