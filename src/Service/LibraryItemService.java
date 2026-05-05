14e37277acf64d297b49b5a9c3c03413369e0762
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 11:55:45 +0400

    in  LibraryItemService

--- src/Service/LibraryItemService.java
+++ src/Service/LibraryItemService.java
@@ -8,4 +8,9 @@ import java.util.List;
 public class LibraryItemService {
     private List<LibraryItem> items =new ArrayList<>();
 
+    public void addedItem(LibraryItem item){
+        items.add(item);
+        System.out.println();
+    }
+
 }
