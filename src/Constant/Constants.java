906d0f32364b736f95f08699f529af7dac62af3b
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 11:16:28 +0400

    Added new messege in Constant

--- src/Constant/Constants.java
+++ src/Constant/Constants.java
@@ -2,5 +2,7 @@ package Constant;
 
 public class Constants {
     public static String ALREADY_CHECKED_OUT="Item is already checked out.";
+    public static  String ERROR_NOT_CHECKED_OUT     = "Item was not checked out.";
+
 
 }
