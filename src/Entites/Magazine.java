8424e3b9c445efcac8dd16a87d145efb8b6d8c21
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:11:42 +0400

    Added constructor  for  fields in  Class Magazine

--- src/Entites/Magazine.java
+++ src/Entites/Magazine.java
@@ -8,4 +8,20 @@ public class Magazine {
         this.issueNumber = issueNumber;
         this.publisher = publisher;
     }
+
+    public String getIssueNumber() {
+        return issueNumber;
+    }
+
+    public void setIssueNumber(String issueNumber) {
+        this.issueNumber = issueNumber;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
 }
