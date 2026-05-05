038b807e1c3b66a05086c4cb6b6074ec255298c3
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 12:54:15 +0400

    Added registerMember method

--- src/Service/MemberService.java
+++ src/Service/MemberService.java
@@ -1,5 +1,7 @@
 package Service;
 
+import Constant.Constants;
+
 import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,5 +11,10 @@ public class MemberService {
 
     public List<Member> getMembers() {
         return members;
+    }
+
+    public void registerMember(Member member){
+        members.add(member);
+        System.out.println(Constants.SUCCESS_MEMBER_REGISTERE);
     }
 }
