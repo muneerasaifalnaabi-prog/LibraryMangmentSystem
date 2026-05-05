619c8ede6d2a01bcd799631c66af9de2613fd4e9
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:24:04 +0400

    Added getter and setter for  fields in  Class Adrress

--- src/Entites/Adrress.java
+++ src/Entites/Adrress.java
@@ -10,4 +10,28 @@ public class Adrress {
                 this.city = city;
                 this.postalCode = postalCode;
         }
+
+        public String getStreet() {
+                return street;
+        }
+
+        public void setStreet(String street) {
+                this.street = street;
+        }
+
+        public String getCity() {
+                return city;
+        }
+
+        public void setCity(String city) {
+                this.city = city;
+        }
+
+        public String getPostalCode() {
+                return postalCode;
+        }
+
+        public void setPostalCode(String postalCode) {
+                this.postalCode = postalCode;
+        }
 }
