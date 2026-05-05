2b6e77350eb990a6dd98f825ba381ec62e10dbad
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:14:19 +0400

    Added constructor  for  fields in  Class LibraryItem

--- src/Entites/LibraryItem.java
+++ src/Entites/LibraryItem.java
@@ -4,4 +4,10 @@ public class LibraryItem {
     private String id;
     private String title ;
     private Boolean status;
+
+    public LibraryItem(String id, String title, Boolean status) {
+        this.id = id;
+        this.title = title;
+        this.status = status;
+    }
 }
