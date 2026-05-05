189a1e48ce312f0074e7a02b2a7cd6f17cf58675
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-05 10:24:41 +0400

    Added constructor  for  fields in  Class Members

--- src/Entites/Members.java
+++ src/Entites/Members.java
@@ -6,4 +6,10 @@ public class Members {
     private Adrress adress;
     private String borrowItem;
 
+    public Members(String name, String id, Adrress adress, String borrowItem) {
+        this.name = name;
+        this.id = id;
+        this.adress = adress;
+        this.borrowItem = borrowItem;
+    }
 }
