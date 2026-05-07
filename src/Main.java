210a136df8ee16b4270266af71239a639534d490
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-05-07 11:43:16 +0400

    Completed Library mangment system with Recursion Menu

--- src/Main.java
+++ src/Main.java
@@ -15,49 +15,9 @@ public class Main {
     static Menu menu = new Menu();
 
     public static void main(String[] args) {
+        handleMainMenu();
 
 
-   /*     Boolean mainMenuContinue = true;
-        while (mainMenuContinue) {
-            System.out.println("Enter Your Choice");
-            menu.displayMenu();
-            Integer option = input.nextInt();
-            switch (option) {
-                case 1 -> {
-                    Boolean itemMenuContinue = true;
-                    while (itemMenuContinue) {
-                        System.out.println(MenuMessages.ITEM_MENU_MESSAGE);
-                        Integer itemOption = input.nextInt();
-                        itemMenuContinue = itemService.handleItemMenu(itemOption);
-
-                    }
-                }
-                case 2 -> {
-                    Boolean memeberMenuContinue = true;
-                    while (memeberMenuContinue) {
-                        System.out.println("-------------Member Menu---------");
-                        System.out.println(MenuMessages.MEMBER_MENU_MESSAGE);
-                        Integer itemOption = input.nextInt();
-                        memeberMenuContinue = memberService.handleMemberMenu(itemOption);
-
-                    }
-                }
-                case 3 -> {
-                    Boolean borrowingMenuContinue = true;
-                    while (borrowingMenuContinue) {
-                        System.out.println(MenuMessages.BORROWING_MENU_MESSAGE);
-                        int borrowingOption = input.nextInt();
-                        borrowingMenuContinue = borrowingService.handleBorrowingMenu(borrowingOption);
-                    }
-                }
-                case 6 -> {
-                    menu.exit();
-                    mainMenuContinue = false;
-                }
-
-            }
-        }
-    }*/
     }
     public static void  handleMainMenu(){
         Menu menu = new Menu();
@@ -93,4 +53,5 @@ public class Main {
         }
     }
         }
+        //completed
 
